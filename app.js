d9f593572c562368d2d6f570f52f76bff7997f85
Iulia Todoran <91527130+iuliatodoran@users.noreply.github.com>
2024-10-11 12:51:45 +0300

    site restaurant

---
+++ app.js
@@ -0,0 +1,11 @@
+function openMeniuPage() {
+    window.location.href = 'Meniul Nostru.html';
+}
+
+function openAboutPage() {
+    window.location.href = 'index.html';
+}
+
+function openContactPage() {
+    window.location.href = 'Contact.html';
+}
